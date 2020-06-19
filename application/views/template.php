1931cdfecba7706183662cc105fb80238fd72ce7
Giani Braga <giani.braga80@gmail.com>
2020-06-19 15:30:58 -0300

    Ajustado e finalizado permissões de usuarios.

--- application/views/template.php
+++ application/views/template.php
@@ -4,6 +4,7 @@ This is a starter template page. Use this page to start your new project from
 scratch. This page gets rid of all links and provides the needed markup only.
 -->
 <html>
+
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -30,8 +31,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <![endif]-->
 
   <!-- Google Font -->
-  <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
 <!--
 BODY TAG OPTIONS:
@@ -53,69 +53,72 @@ desired effect
 |               | sidebar-mini                            |
 |---------------------------------------------------------|
 -->
+
 <body class="hold-transition skin-black sidebar-mini">
-<div class="wrapper">
+  <div class="wrapper">
 
-  <!-- Main Header -->
-  <header class="main-header">
+    <!-- Main Header -->
+    <header class="main-header">
 
-    <!-- Logo -->
-    <a href="<?= site_url('agendamento'); ?>" class="logo">
-      
-      <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b>PET</b></span>
-      <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>Admin Portal Pet</b></span>
-    </a>
+      <!-- Logo -->
+      <a href="<?= site_url('agendamento'); ?>" class="logo">
 
-    <!-- Header Navbar -->
-    <nav class="navbar navbar-static-top" role="navigation">
-      <!-- Sidebar toggle button-->
-      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
-        <span class="sr-only">Toggle navigation</span>
+        <!-- mini logo for sidebar mini 50x50 pixels -->
+        <span class="logo-mini"><b>PET</b></span>
+        <!-- logo for regular state and mobile devices -->
+        <span class="logo-lg"><b>Admin Portal Pet</b></span>
       </a>
-      <!-- Navbar Right Menu -->
-      <div class="navbar-custom-menu">
-        <ul class="nav navbar-nav">
-          <!-- Messages: style can be found in dropdown.less-->
-          <li class="dropdown messages-menu">
-            <!-- Menu toggle button -->
-            </a>
-            <ul class="dropdown-menu">
-              <li>
-                <!-- inner menu: contains the messages -->
-                <ul class="menu">
-                  <li><!-- start message -->
-                    <a href="#">
-                      <div class="pull-left">
-                        <!-- User Image -->
-                      </div>
-                      <!-- Message title and timestamp -->
-                      <h4>
-                        Support Team
-                        <small><i class="fa fa-clock-o"></i> 5 mins</small>
-                      </h4>
-                      <!-- The message -->
-                      <p>Why not buy a new awesome theme?</p>
-                    </a>
-                  </li>
-                  <!-- end message -->
-                </ul>
-                <!-- /.menu -->
-              </li>
-              <li class="footer"><a href="#">See All Messages</a></li>
-            </ul>
-          </li>
-          <!-- /.messages-menu -->
 
-          <!-- Notifications Menu -->
-         
+      <!-- Header Navbar -->
+      <nav class="navbar navbar-static-top" role="navigation">
+        <!-- Sidebar toggle button-->
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+          <span class="sr-only">Toggle navigation</span>
+        </a>
+        <!-- Navbar Right Menu -->
+        <div class="navbar-custom-menu">
+          <ul class="nav navbar-nav">
+            <!-- Messages: style can be found in dropdown.less-->
+            <li class="dropdown messages-menu">
+              <!-- Menu toggle button -->
+              </a>
+              <ul class="dropdown-menu">
+                <li>
+                  <!-- inner menu: contains the messages -->
+                  <ul class="menu">
+                    <li>
+                      <!-- start message -->
+                      <a href="#">
+                        <div class="pull-left">
+                          <!-- User Image -->
+                        </div>
+                        <!-- Message title and timestamp -->
+                        <h4>
+                          Support Team
+                          <small><i class="fa fa-clock-o"></i> 5 mins</small>
+                        </h4>
+                        <!-- The message -->
+                        <p>Why not buy a new awesome theme?</p>
+                      </a>
+                    </li>
+                    <!-- end message -->
+                  </ul>
+                  <!-- /.menu -->
+                </li>
+                <li class="footer"><a href="#">See All Messages</a></li>
+              </ul>
+            </li>
+            <!-- /.messages-menu -->
+
+            <!-- Notifications Menu -->
+
 
             <ul class="dropdown-menu">
               <li>
                 <!-- Inner menu: contains the tasks -->
                 <ul class="menu">
-                  <li><!-- Task item -->
+                  <li>
+                    <!-- Task item -->
                     <a href="#">
                       <!-- Task title and progress text -->
                       <h3>
@@ -125,8 +128,7 @@ desired effect
                       <!-- The progress bar -->
                       <div class="progress xs">
                         <!-- Change the css width attribute to simulate progress -->
-                        <div class="progress-bar progress-bar-aqua" style="width: 20%" role="progressbar"
-                             aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
+                        <div class="progress-bar progress-bar-aqua" style="width: 20%" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
                           <span class="sr-only">20% Complete</span>
                         </div>
                       </div>
@@ -139,156 +141,167 @@ desired effect
                 <a href="#">View all tasks</a>
               </li>
             </ul>
-          </li>
-          <!-- User Account Menu -->
-          <li class="dropdown user user-menu">
-            <!-- Menu Toggle Button -->
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <!-- hidden-xs hides the username on small devices so only the image appears. -->
-              <span class="hidden-xs"><?= $this->session->userdata['logado']['email']; ?></span>
-            </a>
-            <ul class="dropdown-menu">
-              <!-- The user image in the menu -->
-              <!-- Menu Body -->
+            </li>
+            <!-- User Account Menu -->
+            <li class="dropdown user user-menu">
+              <!-- Menu Toggle Button -->
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                <!-- hidden-xs hides the username on small devices so only the image appears. -->
+                <span class="hidden-xs"><?= $this->session->userdata['logado']['email']; ?></span>
+              </a>
+              <ul class="dropdown-menu">
+                <!-- The user image in the menu -->
+                <!-- Menu Body -->
                 <!-- /.row -->
-              </li>
-              <!-- Menu Footer-->
-            </ul>
+            </li>
+            <!-- Menu Footer-->
+          </ul>
           </li>
           <!-- Control Sidebar Toggle Button -->
           <li>
-            <a href="<?= site_url('login/logout');?>" ><i class="fa fa-power-off"></i></a>
+            <a href="<?= site_url('login/logout'); ?>"><i class="fa fa-power-off"></i></a>
           </li>
-        </ul>
-      </div>
-    </nav>
-  </header>
-  <!-- Left side column. contains the logo and sidebar -->
-  <aside class="main-sidebar">
-
-    <!-- sidebar: style can be found in sidebar.less -->
-    <section class="sidebar">
-
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel">
-        <div class="pull-left image">
-          <img>
+          </ul>
         </div>
-        <div class="pull-left info">
-          <p><?= $this->session->userdata['logado']['email']; ?></p>
-          <!-- Status -->
-          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+      </nav>
+    </header>
+    <!-- Left side column. contains the logo and sidebar -->
+    <aside class="main-sidebar">
+
+      <!-- sidebar: style can be found in sidebar.less -->
+      <section class="sidebar">
+
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel">
+          <div class="pull-left image">
+            <img>
+          </div>
+          <div class="pull-left info">
+            <p><?= $this->session->userdata['logado']['email']; ?></p>
+            <!-- Status -->
+            <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+          </div>
         </div>
-      </div>
 
-      <!-- search form (Optional) -->
-      <!-- /.search form -->
+        <!-- search form (Optional) -->
+        <!-- /.search form -->
 
-      <!-- Sidebar Menu -->
-      <ul class="sidebar-menu" data-widget="tree">
-        <li class="header">Menu</li>
-        <!-- Optionally, you can add icons to the links -->
-            <li class="">
-              <a href="<?= site_url('agendamento'); ?>">
-                <i class="fa fa-link"></i> <span>Agendamento</span>
-              </a>
-              <li class="">
-              <a href="<?= site_url('checklist'); ?>">
-                <i class="fa fa-link"></i> <span>Checklist</span>
-              </a>
-              
+        <!-- Sidebar Menu -->
+        <ul class="sidebar-menu" data-widget="tree">
+          <li class="header">Menu</li>
+          <!-- Optionally, you can add icons to the links -->
+          <li class="">
+            <a href="<?= site_url('agendamento'); ?>">
+              <i class="fa fa-link"></i> <span>Agendamento</span>
+            </a>
+          <li class="">
+            <a href="<?= site_url('checklist'); ?>">
+              <i class="fa fa-link"></i> <span>Checklist</span>
+            </a>
 
-      <li class="treeview">
-        <a href="#"><i class="fa fa-link"></i> <span>Relatórios</span>
-          <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-        </a>
-        <ul class="treeview-menu">
-          <li><a href="<?= site_url('relatorio/formAgendamentoPeriodo'); ?>">Relatório Agendamento</a></li>
-        </ul>
-        <li class="treeview">
-          <a href="#"><i class="fa fa-link"></i> <span>Manutenções</span>
-            <span class="pull-right-container">
+
+          <li class="treeview">
+            <a href="#"><i class="fa fa-link"></i> <span>Relatórios</span>
+              <span class="pull-right-container">
                 <i class="fa fa-angle-left pull-right"></i>
               </span>
-          </a>
-          <ul class="treeview-menu">
-           <?php if($this->session->userdata['logado']['permissao']==USUARIO_ADMIN){ ?>
-              <li><a href="<?= site_url('usuario'); ?>">Usuários</a></li>
-            <?php } ?>
-            <li><a href="<?= site_url('funcionario'); ?>">Funcionario</a></li>
-            <li> <a href="<?= site_url('servico'); ?>">Serviço</a></li>
-            <li><a href="<?= site_url('animal'); ?>">Pet</a></li>
-            <li><a href="<?= site_url('pessoa'); ?>">Pessoa Responsavel</a></li>
-            <li><a href="<?= site_url('servico'); ?>">Serviço</a></li>
-            <li><a href="<?= site_url('raca'); ?>">Raça</a></li>
-            <li><a href="<?= site_url('horario'); ?>">Horário</a></li>
-          </ul>
-        </li>
-      </ul>
-      <!-- /.sidebar-menu -->
-    </section>
-    <!-- /.sidebar -->
-  </aside>
+            </a>
+            <ul class="treeview-menu">
+              <li><a href="<?= site_url('relatorio/formAgendamentoPeriodo'); ?>">Relatório Agendamento</a></li>
+            </ul>
+          <li class="treeview">
+            <a href="#"><i class="fa fa-link"></i> <span>Manutenções</span>
+              <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+              </span>
+            </a>
+            <ul class="treeview-menu">
+              <?php if ($this->session->userdata['logado']['permissao'] == USUARIO_ADMIN) { ?>
+                <li><a href="<?= site_url('funcionario'); ?>">Funcionário</a></li>
+              <?php } ?>
+              <?php if ($this->session->userdata['logado']['permissao'] == USUARIO_ADMIN) { ?>
+                <li><a href="<?= site_url('horario'); ?>">Horário</a></li>
+              <?php } ?>
+              <li><a href="<?= site_url('pessoa'); ?>">Pessoa Responsavel</a></li>
+              <li><a href="<?= site_url('animal'); ?>">Pet</a></li>
+              <?php if ($this->session->userdata['logado']['permissao'] == USUARIO_ADMIN) { ?>
+                <li><a href="<?= site_url('raca'); ?>">Raça</a></li>
+              <?php } ?>
+              <?php if ($this->session->userdata['logado']['permissao'] == USUARIO_ADMIN) { ?>
+                <li><a href="<?= site_url('servico'); ?>">Serviço</a></li>
+              <?php } ?>
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1>
-        <?php echo $titulo; ?>
-        <small><?php if(isset($subtitulo)) echo $subtitulo; ?></small>
-      </h1>
-      <!-- <ol class="breadcrumb">
+              <?php if ($this->session->userdata['logado']['permissao'] == USUARIO_ADMIN) { ?>
+                <li><a href="<?= site_url('usuario'); ?>">Usuários</a></li>
+              <?php } ?>
+
+            </ul>
+          </li>
+        </ul>
+        <!-- /.sidebar-menu -->
+      </section>
+      <!-- /.sidebar -->
+    </aside>
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <section class="content-header">
+        <h1>
+          <?php echo $titulo; ?>
+          <small><?php if (isset($subtitulo)) echo $subtitulo; ?></small>
+        </h1>
+        <!-- <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
         <li class="active">Here</li>
       </ol> -->
-    </section>
+      </section>
 
-    <!-- Main content -->
-    <section class="content container-fluid">
+      <!-- Main content -->
+      <section class="content container-fluid">
 
-      <!--------------------------
+        <!--------------------------
         | Your Page Content Here |
         -------------------------->
-        <?php echo $contents; //conteudo da view reportada pela biblioteca de template ?>
+        <?php echo $contents; //conteudo da view reportada pela biblioteca de template 
+        ?>
 
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-
-  <!-- Main Footer -->
-  <footer class="main-footer">
-    <!-- To the right -->
-    <div class="pull-right hidden-xs">
+      </section>
+      <!-- /.content -->
     </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2018 <a href="#">Braga Company</a>.</strong> All rights reserved.
-  </footer>
+    <!-- /.content-wrapper -->
 
-  <!-- Control Sidebar -->
-  <!-- <aside class="control-sidebar control-sidebar-dark">
+    <!-- Main Footer -->
+    <footer class="main-footer">
+      <!-- To the right -->
+      <div class="pull-right hidden-xs">
+      </div>
+      <!-- Default to the left -->
+      <strong>Copyright &copy; 2018 <a href="#">Braga Company</a>.</strong> All rights reserved.
+    </footer>
+
+    <!-- Control Sidebar -->
+    <!-- <aside class="control-sidebar control-sidebar-dark">
     <!-- Create the tabs -->
-  <!-- /.control-sidebar -->
-  <!-- Add the sidebar's background. This div must be placed
+    <!-- /.control-sidebar -->
+    <!-- Add the sidebar's background. This div must be placed
   immediately after the control sidebar -->
-  <div class="control-sidebar-bg"></div>
-</div>
-<!-- ./wrapper -->
+    <div class="control-sidebar-bg"></div>
+  </div>
+  <!-- ./wrapper -->
 
-<!-- REQUIRED JS SCRIPTS -->
+  <!-- REQUIRED JS SCRIPTS -->
 
-<!-- jQuery 3 -->
-<script src="<?= base_url('assets/bower_components/jquery/dist/jquery.min.js'); ?>"></script>
-<!-- Bootstrap 3.3.7 -->
-<script src="<?= base_url('assets/bower_components/bootstrap/dist/js/bootstrap.min.js'); ?>"></script>
-<!-- AdminLTE App -->
-<script src="<?= base_url('assets/dist/js/adminlte.min.js'); ?>"></script>
+  <!-- jQuery 3 -->
+  <script src="<?= base_url('assets/bower_components/jquery/dist/jquery.min.js'); ?>"></script>
+  <!-- Bootstrap 3.3.7 -->
+  <script src="<?= base_url('assets/bower_components/bootstrap/dist/js/bootstrap.min.js'); ?>"></script>
+  <!-- AdminLTE App -->
+  <script src="<?= base_url('assets/dist/js/adminlte.min.js'); ?>"></script>
 
-<!-- Optionally, you can add Slimscroll and FastClick plugins.
+  <!-- Optionally, you can add Slimscroll and FastClick plugins.
      Both of these plugins are recommended to enhance the
      user experience. -->
 </body>
+
 </html>
